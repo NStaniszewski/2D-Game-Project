c0ed56d30daa0370acc3087083adaa68a5d8eb35
NStaniszewski <93354258+NStaniszewski@users.noreply.github.com>
2022-09-08 20:52:00 -0400

    Update Shop.py

--- Shop.py
+++ Shop.py
@@ -5,18 +5,19 @@ ASSETS_FOLDER=os.path.abspath(os.path.dirname(__file__))
 IMAGE_FOLDER=os.path.join(ASSETS_FOLDER,'image_assets')
 
 class shop_item():
-    def __init__(self,x,y,level,description,scale):
+    def __init__(self,x,y,level,description,scale,cost):
         img=pygame.image.load(os.path.join(IMAGE_FOLDER, 'shop_button.png'))
-        img=pygame.transform.scale(img,(x,y))
         width=img.get_width()
         height=img.get_height()
-        self.img=pygame.transform.scale(img,(int(width*scale),int(height*scale)))
+        img=pygame.transform.scale(img,(width,height))
+        self.img=pygame.transform.scale(img,(int(width)*2,int(height)*2))
         self.rect=self.img.get_rect()
         self.rect.topleft=(x,y)
         self.clicked=False
         self.img=img
         self.level=level
         self.desc=description
+        self.cost=cost
         
 
     def shop_spawn(self,screen):
@@ -29,5 +30,15 @@ class shop_item():
         if pygame.mouse.get_pressed()[0]==0:
             self.clicked=False
         screen.blit(self.img,(self.rect.x,self.rect.y))
+        #level display
+        font=pygame.font.SysFont('calibri.ttf',18)
+        level_display=font.render(str(self.level),True,(0,0,0))
+        screen.blit(level_display,(self.rect.x+125,self.rect.y+9))
+        
+        #description Display
+        font2=pygame.font.SysFont('calibri.ttf',12)
+        desc_display=font2.render(str(self.desc),True,(0,0,0))
+        screen.blit(desc_display,(self.rect.x+4,self.rect.y+19))
+        cost_display=font2.render('Cost: $'+str(self.cost),True,(0,0,0))
+        screen.blit(cost_display,(self.rect.x+4,self.rect.y+19+13))
         return buy
-
